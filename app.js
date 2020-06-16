53674aff06cc6bfc9b98c9e43e9db891f2a33d06
Martin Svejda <svejdam13@gmail.com>
2020-06-16 19:52:14 +0100

    update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const data = [
     gender: 'male',
     lookingfor:'female',
     location: 'Boston MA',
-    image: 'https: //randomuser.me/api/portraits/men/82.jps'
+    image: 'https://randomuser.me/api/portraits/men/81.jpg'
   },
   {
     name: 'Jen Smith',
@@ -13,7 +13,7 @@ const data = [
     gender: 'female',
     lookingfor:'male',
     location: 'Miami FL',
-    image: 'https: //randomuser.me/api/portraits/women/82.jps'
+    image: 'https://randomuser.me/api/portraits/women/80.jpg'
   },
   {
     name: 'William Johnson',
@@ -21,27 +21,33 @@ const data = [
     gender: 'male',
     lookingfor:'female',
     location: 'Lynn MA',
-    image: 'https: //randomuser.me/api/portraits/men/82.jps'
+    image: 'https://randomuser.me/api/portraits/men/10.jpg'
   },
 ];
 
-const profiles = profileIterator(data);
-
+const profiles = profileIterator(data); //Tohle je iterator
+// Call first profile (na zacatku);
+nextProfile() //Ta fce se zavola hned po reload webu
 // Next Event
 document.getElementById('next').addEventListener('click', nextProfile);
 // Next Profile Display
 function nextProfile() {
   const currentProfile = profiles.next().value;
-
+if(currentProfile !== undefined) { // undefined to je, kdyz jsi na konci
   document.getElementById('profileDisplay').innerHTML = `
     <ul class="list-group">
       <li class="list-group-item">Name: ${currentProfile.name}</li>
       <li class="list-group-item">Age: ${currentProfile.age}</li>
       <li class="list-group-item">Location: ${currentProfile.location}</li>
       <li class="list-group-item">Preference: ${currentProfile.gender}</li>
-      <li class="list-group-item">Name: ${currentProfile.lookingfor}</li>
+      <li class="list-group-item">Looking for: ${currentProfile.lookingfor}</li>
     </ul>
   `;
+  document.getElementById('imageDisplay').innerHTML =`<img src=${currentProfile.image}>`;
+} else {
+  // no more profiles
+  window.location.reload(); // pokud se dostanes na posledniho user tak se to reloadne
+}
 }
 
 //Profile Iterator
@@ -50,7 +56,9 @@ function profileIterator(profiles){
 
   return {
     next: function() {
-      return nextIndex < profiles.length ? { value: profiles [nextIndex++], done: false } : { done: true }
+      return nextIndex < profiles.length ? 
+      { value: profiles[nextIndex++], done: false } : 
+      { done: true }
     }
-  }
+  };
 }
